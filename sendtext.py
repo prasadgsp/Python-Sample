01a4c8fa710cf82d0304338ec447a686817dbd07
Gowrisankar Prasad <prasad.gowrisankar@gmail.com>
2015-05-26 19:52:20 -0700

    Changed Base Code for Twilio Program

--- sendtext.py
+++ sendtext.py
@@ -5,7 +5,7 @@ account_sid = "AC21bdb75cd16649692c0227bb0ba2def4"
 auth_token  = "5701f8e6d79afee8078d5bed696aa229"
 client = TwilioRestClient(account_sid, auth_token)
  
-message = client.messages.create(body="This is Prasad, I Love you Amma!!!",
+message = client.messages.create(body="This is Prasad, Sample Message sent via Twilio API",
     to="+14803266021",    # Replace with your phone number
     from_="+16237481551") # Replace with your Twilio number
 print message.sid
